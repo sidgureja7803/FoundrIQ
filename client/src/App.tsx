129c5c3a3c86f71d3ac52298548974dcc325cb0f
sidgureja7803 <siddhantgureja39@gmail.com>
2025-11-23 19:23:57 +0530

    Fix scrolling issue and add Vercel configuration

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -17,23 +17,25 @@ import NotFoundPage from './pages/NotFoundPage';
 const App: React.FC = () => {
   return (
     <AuthProvider>
-      <Routes>
-        {/* Public routes */}
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/sign-in" element={<SignInPage />} />
-        <Route path="/sign-up" element={<SignUpPage />} />
-        <Route path="/gallery" element={<GalleryPage />} />
-        <Route path="/terms" element={<TermsPage />} />
-        <Route path="/privacy" element={<PrivacyPage />} />
-        
-        {/* Protected routes */}
-        <Route path="/my-ideas" element={<MyIdeasPage />} />
-        <Route path="/validate-idea" element={<IdeaSubmissionPage />} />
-        <Route path="/idea/:ideaId" element={<IdeaDetailsPage />} />
-        
-        {/* 404 Not found */}
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
+      <div className="overflow-x-hidden">
+        <Routes>
+          {/* Public routes */}
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/sign-in" element={<SignInPage />} />
+          <Route path="/sign-up" element={<SignUpPage />} />
+          <Route path="/gallery" element={<GalleryPage />} />
+          <Route path="/terms" element={<TermsPage />} />
+          <Route path="/privacy" element={<PrivacyPage />} />
+          
+          {/* Protected routes */}
+          <Route path="/my-ideas" element={<MyIdeasPage />} />
+          <Route path="/validate-idea" element={<IdeaSubmissionPage />} />
+          <Route path="/idea/:ideaId" element={<IdeaDetailsPage />} />
+          
+          {/* 404 Not found */}
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </div>
     </AuthProvider>
   );
 };
