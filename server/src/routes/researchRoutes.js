73c85c2a6609705e8cfcfaf63504fd7d124a55db
sidgureja7803 <siddhantgureja39@gmail.com>
2025-11-24 20:44:26 +0530

    Fix deployment config

--- server/src/routes/researchRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { createSSEConnection } from '../controllers/researchController.js';
-
-const router = express.Router();
-
-router.get('/stream/:jobId', createSSEConnection);
-
-export default router;
